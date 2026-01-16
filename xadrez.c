62ac8af85e61f9d7220dca9fcfae6b6c30a0ca67
Edineusa <134114854+edineusafernandes@users.noreply.github.com>
2026-01-16 23:51:49 +0000

    add cavalo

--- xadrez.c
+++ xadrez.c
@@ -2,8 +2,10 @@
 
 int main (){
     int b = 1, r = 1;
+    int c, l = 1;
 
     printf("TABULEIRO\n");
+    printf("\n");
 
     printf("Torre: \n");
     //torre - linha reta horizontalmente ou verticalmente
@@ -11,19 +13,31 @@ int main (){
         printf("Direita\n");
     }
 
+    printf("\n");
     printf("Bispo: \n");
     //bispo - diagonal (cima, direita), 5 casas
     while(b <= 5) {
         printf("Cima, direita\n");
         b++;
     }
-
+    printf("\n");
     //rainha - move-se em todas as direções (8 casas para a esquerda)
         printf("Rainha: \n");    
     do {
            printf("Esquerda\n"); 
            r++;
         } while (r <= 8);
+
+        printf("\n");
+    // cavalo - 2x para baixo + 1x para a esquerda
+    printf("Cavalo: \n");
+    for (c = 1; c <= 2; c++) {
+        printf("Baixo\n");
+        while (l < c) {
+            printf("Esquerda\n");
+            l++;
+        }
+    }
     
     return 0;
 }
